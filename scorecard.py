8727eb306434c74babe40bcf0729fb22ac2a4371
Joel Buchheim-Moore <joelbm24@gmail.com>
2020-07-28 23:58:11 -0700

    started scorecard

--- scorecard.py
+++ scorecard.py
@@ -3,3 +3,27 @@ class Scorecard():
         self.total = 0
         self.subtotal = 0
 
+        self.lower_card = {
+            "aces": None,
+            "twos": None,
+            "threes": None,
+            "fours": None,
+            "fives": None,
+            "sixes": None,
+            "bonus": None
+        }
+
+        self.upper_card = {
+            "triples": None,
+            "quads": None,
+            "full": None,
+            "small": None,
+            "large": None,
+            "yahtzee": None,
+            "chance": None
+        }
+
+    def score(self, dice):
+       pass 
+
+
